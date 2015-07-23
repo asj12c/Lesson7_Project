96d88e5d63f562503814920dd7a27e1bd67c4c8e
Asj12c <asj12c@my.fsu.edu>
2015-07-23 00:39:11 +0000

    Lesson 7 Project - first commit

---
+++ Lesson7_Assignment_Student/script.js
@@ -0,0 +1,33 @@
+/**
+      Project Name:  Recipe Display Application
+      Author: Anthony Jaramillo
+      Date:   07/17/15
+
+      Filename: script.js
+**/ 
+
+function display(event){
+    
+  $(event.currentTarget).next().fadeIn("slow");
+    
+}//end of display()
+
+/*
+$("h3").click(display);
+*/
+
+function display2(event) {
+   $(event.currentTarget).next().animate( { height: 'toggle'}, "slow");
+}
+
+/*
+$("h3").click(display2);
+*/
+
+$("h3").hover(display2);
+
+$("h3").hover(function(){
+       $(this).css("background-color", "yellow");
+       }, function(){
+       $(this).css("background-color", "pink");
+});
